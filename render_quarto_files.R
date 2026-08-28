759b11a1840b944f3916dc703f3b59893357e5d4
Marcelo Araya-Salas <marcelo.araya@ucr.ac.cr>
2026-08-28 11:58:42 -0600

    added videos

--- render_quarto_files.R
+++ render_quarto_files.R
@@ -2,3 +2,7 @@
 styler::style_dir(filetype = "qmd")
 
 quarto::quarto_render()
+
+
+# cd ~/Dropbox/courses_and_workshops/estadistica/aprendizaje_estadistico_2026
+# npx staticrypt docs/videos.html -p "$SP3068" --short -d docs
